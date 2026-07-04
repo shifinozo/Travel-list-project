2ec55e8a1d0d729f3862e29a21289f3a361c7a6c
shifinozo <shifinshifu246@gmail.com>
2026-07-04 10:40:46 +0530

    fix: correct case-sensitive import paths in App.jsx
    Home/AddPlace imports referenced capitalized filenames that don't
    match the actual lowercase files, which builds fine on case-insensitive
    Windows/macOS but fails on Vercel's case-sensitive Linux build.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css'
-import Home from './components/Home'
-import AddPlace from './components/AddPlace'
+import Home from './components/home'
+import AddPlace from './components/addplace'
 import Visited from './components/visited'
 
 function App() {
